2a998f81fbf8f5a4654e0b0081c0d53066dfe38d
mazenswar <mazenswar@protonmail.com>
2018-12-11 19:23:55 -0500

    translator

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,69 @@
 # require modules here
+require "YAML"
+require "pry"
 
-def load_library
+def load_library path
   # code goes here
+  lib = YAML.load_file(path)
+
+
+  newLib = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+
+
+
+  getE = lib.invert
+  getE.each do |k,v|
+    k.each do |e|
+      newLib["get_meaning"][e] = v
+    end
+  end
+
+
+
+  engE ={
+    "angel"	=> "O:)",
+    "angry" =>	">:(",
+    "bored"	=> ":O",
+    "confused" =>	"%)",
+    "embarrased" =>	":$",
+    "fish" =>	"><>",
+    "glasses" =>	"8D",
+    "grinning" =>	"=D",
+    "happy"	=> ":)",
+    "kiss"	=> ":*",
+    "sad" =>	":'(",
+    "surprised" => ":o",
+    "wink" =>	";)"
+  }
+
+  newLib["get_meaning"].each do |k,v|
+    engE.each do |k1,v1|
+      if v == k1
+        newLib["get_emoticon"].merge!({v1 => k})
+      end
+    end
+  end
+newLib
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon path, emoticon
+  lib = load_library path
+  if lib["get_emoticon"].keys.include?(emoticon)
+    lib["get_emoticon"][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
+def get_english_meaning path, emoticon
   # code goes here
+  lib = load_library path
+  if lib["get_meaning"].keys.include?(emoticon)
+    lib["get_meaning"][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
